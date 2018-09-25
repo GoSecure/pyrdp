f97ac2ab5bfe0263a7d94ba54c2b0acf7253b1cf
Émilio Gonzalez <egonzalez@gosecure.ca>
2018-09-25 11:48:14 -0400

    Fixed a bug where wireshark packet dissection failed at the ServerData packet.

--- rdpy/protocol/rdp/t125/gcc.py
+++ rdpy/protocol/rdp/t125/gcc.py
@@ -610,7 +610,7 @@ def writeConferenceCreateResponse(serverData):
     serverDataStream.writeType(serverData)
     
     return (per.writeChoice(0), per.writeObjectIdentifier(t124_02_98_oid),
-            per.writeLength(len(serverDataStream.getvalue()) + 14), per.writeChoice(0x14),
+            per.writeLength(42), per.writeChoice(0x14),
             per.writeInteger16(0x79F3, 1001), per.writeInteger(1), per.writeEnumerates(0),
             per.writeNumberOfSet(1), per.writeChoice(0xc0),
             per.writeOctetStream(h221_sc_key, 4), per.writeOctetStream(serverDataStream.getvalue()))
