60567e4911270a0bf7b9aa102dc8e9fbc21f19af
Francis Labelle <flabelle@gosecure.ca>
2018-10-02 13:37:05 -0400

    Add helper functions for DomainParams

--- rdpy/protocol/mcs/pdu.py
+++ rdpy/protocol/mcs/pdu.py
@@ -42,6 +42,18 @@ class MCSDomainParams:
         self.maxHeight = maxHeight
         self.maxMCSPDUSize = maxMCSPDUSize
         self.protocolVersion = protocolVersion
+    
+    @staticmethod
+    def createTarget(maxChannelIDs, maxUserIDs):
+        return MCSDomainParams(maxChannelIDs, maxUserIDs, 0, 1, 0, 1, 65535, 2)
+
+    @staticmethod
+    def createMinimum():
+        return MCSDomainParams(1, 1, 1, 1, 0, 1, 1056, 2)
+
+    @staticmethod
+    def createMaximum():
+        return MCSDomainParams(65535, 64535, 65535, 1, 0, 1, 65535, 2)
 
 
 
