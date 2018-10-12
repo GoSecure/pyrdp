3cb63995c5ac7d5a7ae20ab692ae8fbc88577587
Francis Labelle <flabelle@gosecure.ca>
2018-10-12 13:24:33 -0400

    Implement all capability classes

--- rdpy/pdu/rdp/capability.py
+++ rdpy/pdu/rdp/capability.py
@@ -84,64 +84,195 @@ class ControlCapability(Capability):
 
 
 class WindowsActivationCapability(Capability):
-    pass
+    def __init__(self, helpKeyFlag, helpKeyIndexFlag, helpExtendedKeyFlag, windowManagerKeyFlag):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_ACTIVATION)
+        self.helpKeyFlag = helpKeyFlag
+        self.helpKeyIndexFlag = helpKeyIndexFlag
+        self.helpExtendedKeyFlag = helpExtendedKeyFlag
+        self.windowManagerKeyFlag = windowManagerKeyFlag
+
 
 class PointerCapability(Capability):
-    pass
+    def __init__(self, colorPointerFlag, colorPointerCacheSize, pointerCacheSize):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_POINTER)
+        self.colorPointerFlag = colorPointerFlag
+        self.colorPointerCacheSize = colorPointerCacheSize
+        self.pointerCacheSize = pointerCacheSize
+
 
 class ShareCapability(Capability):
-    pass
+    def __init__(self, nodeID):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_SHARE)
+        self.nodeID = nodeID
+
 
 class ColorCacheCapabilty(Capability):
-    pass
+    def __init__(self, colorTableCacheSize):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_COLORCACHE)
+        self.colorTableCacheSize = colorTableCacheSize
+
 
 class SoundCapability(Capability):
-    pass
+    def __init__(self, soundFlags):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_SOUND)
+        self.soundFlags = soundFlags
+
 
 class InputCapability(Capability):
-    pass
+    def __init__(self, inputFlags, keyboardLayout, keyboardType, keyboardSubType, keyboardFunctionKey, imeFileName):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_INPUT)
+        self.inputFlags = inputFlags
+        self.keyboardLayout = keyboardLayout
+        self.keyboardType = keyboardType
+        self.keyboardSubType = keyboardSubType
+        self.keyboardFunctionKey = keyboardFunctionKey
+        self.imeFileName = imeFileName
+
 
 class FontCapability(Capability):
-    pass
+    def __init__(self, fontSupportFlags):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_FONT)
+        self.fontSupportFlags = fontSupportFlags
+
 
 class BrushCapability(Capability):
-    pass
+    def __init__(self, brushSupportLevel):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_BRUSH)
+        self.brushSupportLevel = brushSupportLevel
+
 
 class GlyphCacheCapability(Capability):
-    pass
+    def __init__(self, glyphCache, fragCache, glyphSupportLevel):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_GLYPHCACHE)
+        self.glyphCache = glyphCache
+        self.fragCache = fragCache
+        self.glyphSupportLevel = glyphSupportLevel
+
 
 class OffscreenBitmapCacheCapability(Capability):
-    pass
+    def __init__(self, offscreenSupportLevel, offscreenCacheSize, offscreenCacheEntries):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_OFFSCREENCACHE)
+        self.offscreenSupportLevel = offscreenSupportLevel
+        self.offscreenCacheSize = offscreenCacheSize
+        self.offscreenCacheEntries = offscreenCacheEntries
+
+
+class BitmapCacheHostSupportCapability(Capability):
+    def __init__(self, cacheVersion):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_BITMAPCACHE_HOSTSUPPORT)
+        self.cacheVersion = cacheVersion
+
 
 class VirtualChannelCapability(Capability):
-    pass
+    def __init__(self, flags, vcChunkSize):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_VIRTUALCHANNEL)
+        self.flags = flags
+        self.vcChunkSize = vcChunkSize
+
 
 class DrawNineGridCacheCapability(Capability):
-    pass
+    def __init__(self, drawNineGridSupportLevel, drawNineGridCacheSize, drawNineGridCacheEntries):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_DRAWNINEGRIDCACHE)
+        self.drawNineGridSupportLevel = drawNineGridSupportLevel
+        self.drawNineGridCacheSize = drawNineGridCacheSize
+        self.drawNineGridCacheEntries = drawNineGridCacheEntries
+
 
 class DrawGDIPlusCapability(Capability):
-    pass
+    def __init__(self, drawGDIPlusSupportLevel, gdipVersion, drawGDIPlusCacheLevel, gdipCacheEntries, gdipCacheChunkSize, gdipImageCacheProperties):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_DRAWGDIPLUS)
+        self.drawGDIPlusSupportLevel = drawGDIPlusSupportLevel
+        self.gdipVersion = gdipVersion
+        self.drawGDIPlusCacheLevel = drawGDIPlusCacheLevel
+        self.gdipCacheEntries = gdipCacheEntries
+        self.gdipCacheChunkSize = gdipCacheChunkSize
+        self.gdipImageCacheProperties = gdipImageCacheProperties
+
 
 class RemoteProgramsCapability(Capability):
-    pass
+    def __init__(self, railSupportLevel):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_RAIL)
+        self.railSupportLevel = railSupportLevel
+
 
 class WindowListCapability(Capability):
-    pass
+    def __init__(self, wndSupportLevel, numIconCaches, numIconCacheEntries):
+        Capability.__init__(self, CapabilityType.CAPSTYPE_WINDOW)
+        self.wndSupportLevel = wndSupportLevel
+        self.numIconCaches = numIconCaches
+        self.numIconCacheEntries = numIconCacheEntries
+
 
 class DesktopCompositionCapability(Capability):
-    pass
+    def __init__(self, compDeskSupportLevel):
+        Capability.__init__(self, CapabilityType.CAPSETTYPE_COMPDESK)
+        self.compDeskSupportLevel = compDeskSupportLevel
+
 
 class MultifragmentUpdateCapability(Capability):
-    pass
+    def __init__(self, maxRequestSize):
+        Capability.__init__(self, CapabilityType.CAPSETTYPE_MULTIFRAGMENTUPDATE)
+        self.maxRequestSize = maxRequestSize
+
 
 class LargePointerCapability(Capability):
-    pass
+    def __init__(self, largePointerSupportFlags):
+        Capability.__init__(self, CapabilityType.CAPSETTYPE_LARGE_POINTER)
+        self.largePointerSupportFlags = largePointerSupportFlags
+
 
 class SurfaceCommandsCapability(Capability):
-    pass
+    def __init__(self, cmdFlags, reserved):
+        Capability.__init__(self, CapabilityType.CAPSETTYPE_SURFACE_COMMANDS)
+        self.cmdFlags = cmdFlags
+        self.reserved = reserved
+
 
 class BitmapCodecsCapability(Capability):
-    pass
+    def __init__(self, supportedBitmapCodecs):
+        Capability.__init__(self, CapabilityType.CAPSETTYPE_BITMAP_CODECS)
+        self.supportedBitmapCodecs = supportedBitmapCodecs
+
+
+class BitmapCodec:
+    def __init__(self, guid, id, properties):
+        self.guid = guid
+        self.id = id
+        self.properties = properties
+
+
+class NSCodec:
+    def __init__(self, allowDynamicFidelity, allowSubsampling, colorLossLevel):
+        self.allowDynamicFidelity = allowDynamicFidelity
+        self.allowSubsampling = allowSubsampling
+        self.colorLossLevel = colorLossLevel
+
+
+class ClientCapsContainer:
+    def __init__(self, captureFlags, rfxCapset):
+        self.captureFlags = captureFlags
+        self.rfxCaps = rfxCapset
+
+
+class RFXCapset:
+    def __init__(self, id, capsetType, icaps):
+        self.id = id
+        self.capsetType = capsetType
+        self.icaps = icaps
+
+
+class RFXIcap:
+    def __init__(self, flags, entropyBits):
+        self.flags = flags
+        self.entropyBits = entropyBits
+        
+
+class ServerCapsContainer:
+    def __init__(self, reserved):
+        self.reserved = reserved
+
 
 class FrameAcknowledgeCapability(Capability):
-    pass
+    def __init__(self, maxUnacknowledgedFrameCount):
+        Capability.__init__(self, CapabilityType.CAPSSETTYPE_FRAME_ACKNOWLEDGE)
+        self.maxUnacknowledgedFrameCount = maxUnacknowledgedFrameCount
