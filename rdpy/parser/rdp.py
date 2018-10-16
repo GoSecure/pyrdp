2f09f28ea1d11a06b0d2a026570057ba42e939b8
Émilio Gonzalez <egonzalez@gosecure.ca>
2018-10-16 17:18:12 -0400

    - Added documentation for some RDP Licensing parser

--- rdpy/parser/rdp.py
+++ rdpy/parser/rdp.py
@@ -228,12 +228,21 @@ class RDPSecurityParser:
 
 
 class RDPLicensingParser:
+    """
+    Parse the RDP Licensing part of the RDP connection sequence
+    """
+
     def __init__(self):
         self.parsers = {
             RDPLicensingPDUType.ERROR_ALERT: self.parseErrorAlert,
         }
 
     def parse(self, data):
+        """
+        Read the provided byte stream and return the corresponding RDPLicensingPDU.
+        :type data: str
+        :return: RDPLicensingPDU
+        """
 
         stream = StringIO(data)
         header = Uint8.unpack(stream)
@@ -246,18 +255,34 @@ class RDPLicensingParser:
         return self.parsers[header](stream, flags)
 
     def parseLicenseBlob(self, stream):
+        """
+        Parse the provided byte stream and return the corresponding RDPLicenseBinaryBlob
+        :type stream: StringIO
+        :return: RDPLicenseBinaryBlob
+        """
         type = Uint16LE.unpack(stream)
         length = Uint16LE.unpack(stream)
         data = stream.read(length)
         return RDPLicenseBinaryBlob(type, data)
 
     def parseErrorAlert(self, stream, flags):
+        """
+        Parse the provided byte stream and return the corresponding RDPLicenseErrorAlertPDU
+        :type stream: StringIO
+        :param flags: The flags of the Licencing PDU.
+        :return: RDPLicenseErrorAlertPDU
+        """
         errorCode = Uint32LE.unpack(stream)
         stateTransition = Uint32LE.unpack(stream)
         blob = self.parseLicenseBlob(stream)
         return RDPLicenseErrorAlertPDU(flags, errorCode, stateTransition, blob)
 
     def write(self, pdu):
+        """
+        Encode a RDPLicensingPDU into a byte stream to send to the previous layer.
+        :type pdu: rdpy.pdu.rdp.licensing.RDPLicensingPDU
+        :return: RDPLicensingPDU
+        """
         stream = StringIO()
         stream.write(Uint8.pack(pdu.header))
         stream.write(Uint8.pack(pdu.flags))
