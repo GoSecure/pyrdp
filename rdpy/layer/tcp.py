3531a83d38f8cfb477e44ab93f97dc15386718a0
Émilio Gonzalez <egonzalez@gosecure.ca>
2018-10-16 10:27:36 -0400

    Added documentation for TCP layer

--- rdpy/layer/tcp.py
+++ rdpy/layer/tcp.py
@@ -10,20 +10,38 @@ class TCPLayer(Protocol, Layer):
     """
     Twisted protocol class and first layer in a stack.
     ObservedBy: TCPObserver
-    Never notifies observers about PDUs because there isn't really a TCP PDU type per say.
+    Never notifies observers about PDUs because there isn't really a TCP PDU type per se.
     TCP observers are notified when a connection is made.
     """
     def __init__(self):
         Layer.__init__(self)
 
     def connectionMade(self):
+        """
+        When the TCP handshake is completed, notify the observer.
+        """
         self.observer.onConnection()
 
     def dataReceived(self, data):
+        """
+        When a PSH TCP packet is received, call the next layer to receive the data.
+        :param data: The byte stream (without the TCP header)
+        :type data: str
+        """
         self.next.recv(data)
 
     def send(self, data):
+        """
+        Send a TCP packet (or more than one if needed)
+        :param data: The data to send
+        :type data: str
+        """
         self.transport.write(data)
 
-    def startTLS(self, ctx):
-        self.transport.startTLS(ctx)
+    def startTLS(self, context):
+        """
+        Instruct Twisted to make the TLS handshake so that all further communications are encrypted.
+        :param context: Twisted TLS Context object (like DefaultOpenSSLContextFactory)
+        :type context: ServerTLSContext
+        """
+        self.transport.startTLS(context)
