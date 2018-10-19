f529d9d1003af93decb6b724546bbc6abab8609b
Francis Labelle <francis.labelle22@gmail.com>
2018-10-19 14:08:48 -0400

    Restore the argument parsing we had previously

--- bin/rdpy-rdpmitm.py
+++ bin/rdpy-rdpmitm.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+import argparse
 import logging
 
 from twisted.internet import reactor
@@ -18,7 +20,38 @@ class MITMServerFactory(ServerFactory):
         server = MITMServer(self.targetHost, self.targetPort, self.certificateFileName, self.privateKeyFileName)
         return server.getProtocol()
 
+if __name__ == "__main__":
+    log.get_logger().setLevel(logging.DEBUG)
 
-log.get_logger().setLevel(logging.DEBUG)
-reactor.listenTCP(3388, MITMServerFactory("127.0.0.2", 3390, None, None))
-reactor.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", help="IP:port of the target RDP machine (ex: 129.168.0.2:3390)")
+    parser.add_argument("-l", "--listen", help="Port number to listen to. Default 3389", default=3389)
+    parser.add_argument("-o", "--output", help="Output folder for .rss files")
+    parser.add_argument("-i", "--destination-ip", help="Destination IP address to send RDP events to (for live player)."
+                                                       " If not specified, doesn't send the RDP events "
+                                                       "over the network.")
+    parser.add_argument("-d", "--destination-port", help="Destination port number (for live player). Default 3000",
+                        default=3000)
+    parser.add_argument("-k", "--private-key", help="Path to private key (for SSL)")
+    parser.add_argument("-c", "--certificate", help="Path to certificate (for SSL)")
+    parser.add_argument("-r", "--standard-security", help="RDP standard security (XP or server 2003 client or older)",
+                        action="store_true")
+    parser.add_argument("-n", "--nla", help="For NLA client authentication (need to provide credentials)",
+                        action="store_true")
+    parser.add_argument("-u", "--username", help="Username to use to connect to the target VM (instead of the username "
+                                                 "the client sent)")
+    parser.add_argument("-p", "--password", help="Password to use to connect to the target VM (instead of the password "
+                                                 "the client sent)")
+
+    args = parser.parse_args()
+    target = args.target
+
+    if ":" in target:
+        targetHost = target[: target.index(":")]
+        targetPort = int(target[target.index(":") + 1 :])
+    else:
+        targetHost = target
+        targetPort = 3389
+
+    reactor.listenTCP(int(args.listen), MITMServerFactory(targetHost, targetPort, None, None))
+    reactor.run()
