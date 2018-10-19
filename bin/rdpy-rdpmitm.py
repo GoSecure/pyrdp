d5eda18ec0da3051033c6d82e67e9df8b2a1acf1
Francis Labelle <francis.labelle22@gmail.com>
2018-10-19 11:05:41 -0400

    Pass the targetHost and targetPort params to MITMServer

--- bin/rdpy-rdpmitm.py
+++ bin/rdpy-rdpmitm.py
@@ -8,15 +8,17 @@ from rdpy.mitm.server import MITMServer
 
 
 class MITMServerFactory(ServerFactory):
-    def __init__(self, targetIP, privateKeyFileName, certificateFileName):
-        self._privateKeyFileName = privateKeyFileName
-        self._certificateFileName = certificateFileName
+    def __init__(self, targetHost, targetPort, privateKeyFileName, certificateFileName):
+        self.targetHost = targetHost
+        self.targetPort = targetPort
+        self.privateKeyFileName = privateKeyFileName
+        self.certificateFileName = certificateFileName
 
     def buildProtocol(self, addr):
-        server = MITMServer("127.0.0.2", 3390, self._certificateFileName, self._privateKeyFileName)
+        server = MITMServer(self.targetHost, self.targetPort, self.certificateFileName, self.privateKeyFileName)
         return server.getProtocol()
 
 
 log.get_logger().setLevel(logging.DEBUG)
-reactor.listenTCP(3388, MITMServerFactory("127.0.0.1", None, None))
+reactor.listenTCP(3388, MITMServerFactory("127.0.0.2", 3390, None, None))
 reactor.run()
