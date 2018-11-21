e2ea68f3fb5d9ef33098a16281992b7e243dd134
Francis Labelle <flabelle@gosecure.ca>
2018-11-21 14:47:30 -0500

    use timed rotation instead

--- bin/rdpy-rdpmitm.py
+++ bin/rdpy-rdpmitm.py
@@ -97,7 +97,7 @@ def prepare_loggers():
     formatter = logging.Formatter("[%(asctime)s] - %(name)s - %(levelname)s - %(message)s")
 
     stream_handler = logging.StreamHandler()
-    file_handler = logging.handlers.RotatingFileHandler("log/mitm.log", maxBytes=10 * 1024 * 1024, backupCount=10)
+    file_handler = logging.handlers.TimedRotatingFileHandler("log/mitm.log", when="D")
     stream_handler.setFormatter(formatter)
     file_handler.setFormatter(formatter)
     mitm_logger.addHandler(stream_handler)
