1881ba2add46a7577f3347b1de141b496868d670
Francis Labelle <flabelle@gosecure.ca>
2018-11-23 15:42:52 -0500

    Add observer class for message layer

--- rdpy/layer/recording.py
+++ rdpy/layer/recording.py
@@ -1,12 +1,43 @@
 import time
 from io import BytesIO
 
-from rdpy.core.layer import Layer
+from rdpy.core.layer import Layer, LayerRoutedObserver
+from rdpy.core.observer import Observer
 from rdpy.core.packing import Uint8, Uint64LE
+from rdpy.core.subject import ObservedBy
 from rdpy.enum.rdp import RDPPlayerMessageType
 from rdpy.pdu.rdp.recording import RDPPlayerMessagePDU
 
+class RDPPlayerMessageObserver(LayerRoutedObserver):
+    def __index__(self, **kwargs):
+        LayerRoutedObserver.__init__(self, {
+            RDPPlayerMessageType.CONNECTION_CLOSE: "onConnectionClose",
+            RDPPlayerMessageType.CLIENT_INFO: "onClientInfo",
+            RDPPlayerMessageType.CONFIRM_ACTIVE: "onConfirmActive",
+            RDPPlayerMessageType.INPUT: "onInput",
+            RDPPlayerMessageType.OUTPUT: "onOutput",
+            RDPPlayerMessageType.CLIPBOARD_DATA: "onClipboardData",
+        }, **kwargs)
 
+    def onConnectionClose(self, pdu):
+        pass
+
+    def onClientInfo(self, pdu):
+        pass
+
+    def onConfirmActive(self, pdu):
+        pass
+
+    def onInput(self, pdu):
+        pass
+
+    def onOutput(self, pdu):
+        pass
+
+    def onClipboardData(self, pdu):
+        pass
+
+@ObservedBy(RDPPlayerMessageObserver)
 class RDPPlayerMessageLayer(Layer):
     """
     Layer to manage the encapsulation of Player metadata such as event timestamp and
