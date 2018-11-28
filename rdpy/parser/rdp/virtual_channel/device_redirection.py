fbe5c98a2da459b1ed193341079e2a46fabca3f3
Émilio Gonzalez <egonzalez@gosecure.ca>
2018-11-28 13:45:37 -0500

    Add PDUs and parsing for create/close requests/responses for device redirection

--- rdpy/parser/rdp/virtual_channel/device_redirection.py
+++ rdpy/parser/rdp/virtual_channel/device_redirection.py
@@ -5,7 +5,8 @@ from rdpy.enum.virtual_channel.device_redirection import DeviceRedirectionCompon
     MajorFunction
 from rdpy.parser.parser import Parser
 from rdpy.pdu.rdp.virtual_channel.device_redirection import DeviceRedirectionPDU, DeviceIOResponsePDU, \
-    DeviceIORequestPDU, DeviceReadResponsePDU, DeviceReadRequestPDU
+    DeviceIORequestPDU, DeviceReadResponsePDU, DeviceReadRequestPDU, DeviceCreateResponsePDU, DeviceCreateRequestPDU, \
+    DeviceCloseRequestPDU
 
 
 class DeviceRedirectionParser(Parser):
@@ -21,7 +22,9 @@ class DeviceRedirectionParser(Parser):
         }
 
         self.ioRequestParsers = {
-            MajorFunction.IRP_MJ_READ: self.parseDeviceReadRequest
+            MajorFunction.IRP_MJ_READ: self.parseDeviceReadRequest,
+            MajorFunction.IRP_MJ_CREATE: self.parseDeviceCreateRequest,
+            MajorFunction.IRP_MJ_CLOSE: self.parseDeviceCloseRequest
         }
 
     def parse(self, data: bytes):
@@ -48,20 +51,50 @@ class DeviceRedirectionParser(Parser):
         majorFunction = MajorFunction(Uint32LE.unpack(stream))
         minorFunction = Uint32LE.unpack(stream)
         if majorFunction in self.ioRequestParsers.keys():
-            return self.ioRequestParsers[majorFunction](deviceId, fileId, completionId, majorFunction,
+            return self.ioRequestParsers[majorFunction](deviceId, fileId, completionId,
                                                         minorFunction, stream)
         else:
             return DeviceIORequestPDU(deviceId, fileId, completionId, majorFunction, minorFunction)
 
-    def parseDeviceReadRequest(self, deviceId: int, fileId: int, completionId: int, majorFunction: int,
+    def parseDeviceReadRequest(self, deviceId: int, fileId: int, completionId: int,
                                minorFunction: int, stream: BytesIO) -> DeviceReadRequestPDU:
         """
         Starting at length, just before offset
         """
         length = Uint32LE.unpack(stream)
         offset = Uint64LE.unpack(stream)
-        return DeviceReadRequestPDU(deviceId, fileId, completionId, majorFunction, minorFunction,
+        return DeviceReadRequestPDU(deviceId, fileId, completionId, minorFunction,
                                     length, offset)
+
+    def parseDeviceCreateRequest(self, deviceId: int, fileId: int, completionId: int,
+                                 minorFunction: int, stream: BytesIO) -> DeviceCreateRequestPDU:
+        """
+        Starting at desiredAccess.
+        """
+        desiredAccess = Uint32LE.unpack(stream)
+        allocationSize = Uint64LE.unpack(stream)
+        fileAttributes = Uint32LE.unpack(stream)
+        sharedAccess = Uint32LE.unpack(stream)
+        createDisposition = Uint32LE.unpack(stream)
+        createOptions = Uint32LE.unpack(stream)
+        pathLength = Uint32LE.unpack(stream)
+        path = stream.read(pathLength)
+        return DeviceCreateRequestPDU(deviceId, fileId, completionId, minorFunction, desiredAccess, allocationSize, fileAttributes,
+                                      sharedAccess, createDisposition, createOptions, path)
+
+    def parseDeviceCloseRequest(self, deviceId: int, fileId: int, completionId: int,
+                                minorFunction: int, stream: BytesIO) -> DeviceCloseRequestPDU:
+        return DeviceCloseRequestPDU(deviceId, fileId, completionId, minorFunction)
+
+    def parseDeviceCreateResponse(self, pdu: DeviceIOResponsePDU) -> DeviceCreateResponsePDU:
+        """
+        The information field is not yet parsed (it's optional).
+        This one is a bit special since we need to look at previous packet before parsing it as
+        a read response, and we need the packet data.
+        """
+        stream = BytesIO(pdu.payload)
+        fileId = Uint32LE.unpack(stream)
+        return DeviceCreateResponsePDU(pdu.deviceId, pdu.completionId, pdu.ioStatus, fileId)
 
     def parseDeviceIOResponse(self, stream: BytesIO) -> DeviceIOResponsePDU:
         """
@@ -82,7 +115,6 @@ class DeviceRedirectionParser(Parser):
         length = Uint32LE.unpack(stream)
         readData = stream.read(length)
         return DeviceReadResponsePDU(pdu.deviceId, pdu.completionId, pdu.ioStatus, readData)
-
 
     def write(self, pdu):
         raise NotImplementedError("Writing for device redirection packets has not been implemented yet.")
--- rdpy/pdu/rdp/virtual_channel/device_redirection.py
+++ rdpy/pdu/rdp/virtual_channel/device_redirection.py
@@ -1,4 +1,5 @@
-from rdpy.enum.virtual_channel.device_redirection import DeviceRedirectionComponent, DeviceRedirectionPacketId
+from rdpy.enum.virtual_channel.device_redirection import DeviceRedirectionComponent, DeviceRedirectionPacketId, \
+    MajorFunction
 from rdpy.pdu.base_pdu import PDU
 
 
@@ -57,8 +58,55 @@ class DeviceReadRequestPDU(DeviceIORequestPDU):
     https://msdn.microsoft.com/en-us/library/cc241330.aspx
     """
 
-    def __init__(self, deviceId: int, fileId: int, completionId: int, majorFunction: int, minorFunction: int,
+    def __init__(self, deviceId: int, fileId: int, completionId: int, minorFunction: int,
                  length: int, offset: int):
-        super().__init__(deviceId, fileId, completionId, majorFunction, minorFunction)
+        super().__init__(deviceId, fileId, completionId, MajorFunction.IRP_MJ_READ, minorFunction)
         self.length = length
         self.offset = offset
+
+
+class DeviceCreateRequestPDU(DeviceIORequestPDU):
+    """
+    https://msdn.microsoft.com/en-us/library/cc241328.aspx
+    """
+
+    def __init__(self, deviceId: int, fileId: int, completionId: int, minorFunction: int,
+                 desiredAccess: int, allocationSize: int, fileAttributes: int, sharedAccess: int,
+                 createDisposition: int, createOptions: int, path: bytes):
+        super().__init__(deviceId, fileId, completionId, MajorFunction.IRP_MJ_CREATE, minorFunction)
+        self.desiredAccess = desiredAccess
+        self.allocationSize = allocationSize
+        self.fileAttributes = fileAttributes
+        self.sharedAccess = sharedAccess
+        self.createDisposition = createDisposition
+        self.createOptions = createOptions
+        self.path = path
+
+
+class DeviceCreateResponsePDU(DeviceIOResponsePDU):
+    """
+    https://msdn.microsoft.com/en-us/library/cc241335.aspx
+    """
+
+    def __init__(self, deviceId: int, completionId: int, ioStatus: int, fileId: int, information: bytes=0):
+        super().__init__(deviceId, completionId, ioStatus)
+        self.fileId = fileId
+        self.information = information
+
+
+class DeviceCloseRequestPDU(DeviceIORequestPDU):
+    """
+    https://msdn.microsoft.com/en-us/library/cc241329.aspx
+    """
+
+    def __init__(self, deviceId: int, fileId: int, completionId: int, minorFunction: int):
+        super().__init__(deviceId, fileId, completionId, MajorFunction.IRP_MJ_CLOSE, minorFunction)
+
+
+class DeviceCloseResponsePDU(DeviceIOResponsePDU):
+    """
+    https://msdn.microsoft.com/en-us/library/cc241336.aspx
+    """
+
+    def __init__(self, deviceId: int, completionId: int, ioStatus: int):
+        super().__init__(deviceId, completionId, ioStatus)
