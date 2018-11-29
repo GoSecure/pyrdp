2de7cec514f16a49405ee9a4d95d7271316538f7
Émilio Gonzalez <egonzalez@gosecure.ca>
2018-11-29 15:11:44 -0500

    Add write methods to the DeviceRedirection parser

--- rdpy/parser/rdp/virtual_channel/device_redirection.py
+++ rdpy/parser/rdp/virtual_channel/device_redirection.py
@@ -1,12 +1,14 @@
 from io import BytesIO
+from typing import Dict
 
-from rdpy.core.packing import Uint16LE, Uint32LE, Uint64LE
+from rdpy.core import log
+from rdpy.core.packing import Uint16LE, Uint32LE, Uint64LE, Uint8
 from rdpy.enum.virtual_channel.device_redirection import DeviceRedirectionComponent, DeviceRedirectionPacketId, \
     MajorFunction
 from rdpy.parser.parser import Parser
 from rdpy.pdu.rdp.virtual_channel.device_redirection import DeviceRedirectionPDU, DeviceIOResponsePDU, \
     DeviceIORequestPDU, DeviceReadResponsePDU, DeviceReadRequestPDU, DeviceCreateResponsePDU, DeviceCreateRequestPDU, \
-    DeviceCloseRequestPDU
+    DeviceCloseRequestPDU, DeviceCloseResponsePDU
 
 
 class DeviceRedirectionParser(Parser):
@@ -16,30 +18,45 @@ class DeviceRedirectionParser(Parser):
 
     def __init__(self):
         super().__init__()
-        self.parsers = {
+        self.parsers: Dict[DeviceRedirectionPacketId, callable] = {
             DeviceRedirectionPacketId.PAKID_CORE_DEVICE_IOCOMPLETION: self.parseDeviceIOResponse,
             DeviceRedirectionPacketId.PAKID_CORE_DEVICE_IOREQUEST: self.parseDeviceIORequest
         }
+        self.writers: Dict[DeviceRedirectionPacketId, callable] = {
+            DeviceRedirectionPacketId.PAKID_CORE_DEVICE_IOREQUEST: self.writeDeviceIORequest,
+            DeviceRedirectionPacketId.PAKID_CORE_DEVICE_IOCOMPLETION: self.writeDeviceIOResponse
+        }
 
-        self.ioRequestParsers = {
+        self.ioRequestParsers: Dict[MajorFunction, callable] = {
             MajorFunction.IRP_MJ_READ: self.parseDeviceReadRequest,
             MajorFunction.IRP_MJ_CREATE: self.parseDeviceCreateRequest,
             MajorFunction.IRP_MJ_CLOSE: self.parseDeviceCloseRequest
         }
+        self.ioRequestWriters: Dict[type, callable] = {
+            DeviceCreateRequestPDU: self.writeCreateRequest,
+            DeviceCloseRequestPDU: self.writeCloseRequest,
+            DeviceReadRequestPDU: self.writeReadRequest
+        }
 
-    def parse(self, data: bytes):
+        self.ioResponseWriters: Dict[type, callable] = {
+            DeviceCreateResponsePDU: self.writeCreateResponse,
+            DeviceCloseResponsePDU: self.writeCloseResponse,
+            DeviceReadResponsePDU: self.writeReadResponse,
+        }
+
+    def parse(self, data: bytes) -> DeviceRedirectionPDU:
         stream = BytesIO(data)
         unpack = Uint16LE.unpack(stream)
         component = DeviceRedirectionComponent(unpack)
         packetId = DeviceRedirectionPacketId(Uint16LE.unpack(stream))
 
         if component == DeviceRedirectionComponent.RDPDR_CTYP_PRN:
-            raise NotImplementedError("Printer components are not handled yet.")
+            log.warning("Received Printing component packets, which are not handled. Might cause a crash.")
 
         if packetId in self.parsers.keys():
             return self.parsers[packetId](stream)
         else:
-            return DeviceRedirectionPDU(component, packetId)
+            return DeviceRedirectionPDU(component, packetId, payload=stream.read())
 
     def parseDeviceIORequest(self, stream: BytesIO) -> DeviceIORequestPDU:
         """
@@ -116,5 +133,62 @@ class DeviceRedirectionParser(Parser):
         readData = stream.read(length)
         return DeviceReadResponsePDU(pdu.deviceId, pdu.completionId, pdu.ioStatus, readData)
 
-    def write(self, pdu):
-        raise NotImplementedError("Writing for device redirection packets has not been implemented yet.")
+    def write(self, pdu: DeviceRedirectionPDU):
+        stream = BytesIO()
+        Uint16LE.pack(pdu.component, stream)
+        Uint16LE.pack(pdu.packetId, stream)
+        if pdu.packetId in self.writers.keys():
+            self.writers[pdu.packetId](pdu, stream)
+        else:
+            stream.write(pdu.payload)
+        return stream.getvalue()
+
+    def writeDeviceIORequest(self, pdu: DeviceIORequestPDU, stream: BytesIO):
+        Uint32LE.pack(pdu.deviceId, stream)
+        Uint32LE.pack(pdu.fileId, stream)
+        Uint32LE.pack(pdu.completionId, stream)
+        Uint32LE.pack(pdu.majorFunction, stream)
+        Uint32LE.pack(pdu.minorFunction, stream)
+        if type(pdu) in self.ioRequestWriters.keys():
+            self.ioRequestWriters[type(pdu)](pdu, stream)
+        else:
+            stream.write(pdu.payload)
+
+    def writeDeviceIOResponse(self, pdu: DeviceIOResponsePDU, stream: BytesIO):
+        Uint32LE.pack(pdu.deviceId, stream)
+        Uint32LE.pack(pdu.completionId, stream)
+        Uint32LE.pack(pdu.ioStatus, stream)
+        if type(pdu) in self.ioRequestWriters.keys():
+            self.ioResponseWriters[type(pdu)](pdu, stream)
+        else:
+            stream.write(pdu.payload)
+
+    def writeCreateRequest(self, pdu: DeviceCreateRequestPDU, stream: BytesIO):
+        Uint32LE.pack(pdu.desiredAccess, stream)
+        Uint64LE.pack(pdu.allocationSize, stream)
+        Uint32LE.pack(pdu.fileAttributes, stream)
+        Uint32LE.pack(pdu.sharedAccess, stream)
+        Uint32LE.pack(pdu.createDisposition, stream)
+        Uint32LE.pack(pdu.createOptions, stream)
+        Uint32LE.pack(len(pdu.path), stream)
+        stream.write(pdu.path)
+
+    def writeCloseRequest(self, pdu: DeviceCloseRequestPDU, stream: BytesIO):
+        stream.write(b"\x00"*32)  # Padding
+
+    def writeReadRequest(self, pdu: DeviceReadRequestPDU, stream: BytesIO):
+        Uint32LE.pack(pdu.length, stream)
+        Uint32LE.pack(pdu.offset, stream)
+        stream.write(b"\x00" * 20)  # Padding
+
+    def writeCreateResponse(self, pdu: DeviceCreateResponsePDU, stream: BytesIO):
+        Uint32LE.pack(pdu.fileId)
+        if pdu.information is not None:
+            Uint8.pack(pdu.information)
+
+    def writeCloseResponse(self, pdu: DeviceCloseResponsePDU, stream: BytesIO):
+        stream.write(b"\x00" * 4)  # Padding
+
+    def writeReadResponse(self, pdu: DeviceReadResponsePDU, stream: BytesIO):
+        Uint32LE.pack(len(pdu.readData))
+        stream.write(pdu.readData)
