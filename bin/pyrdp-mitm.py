0a8873aad1ad2da846664e3b71533bbb6ce734f6
Francis Labelle <flabelle@gosecure.ca>
2018-12-11 17:02:12 -0500

    Update help strings

--- bin/pyrdp-mitm.py
+++ bin/pyrdp-mitm.py
@@ -107,27 +107,18 @@ def prepare_loggers(logLevel):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("target", help="IP:port of the target RDP machine (ex: 129.168.0.2:3390)")
-    parser.add_argument("-l", "--listen", help="Port number to listen to. Default 3389", default=3389)
-    parser.add_argument("-o", "--output", help="Output folder for .rss files")
-    parser.add_argument("-i", "--destination-ip", help="Destination IP address to send RDP events to (for live player)."
-                                                       " If not specified, doesn't send the RDP events "
-                                                       "over the network.")
-    parser.add_argument("-d", "--destination-port", help="Destination port number (for live player). Default 3000",
-                        default=3000)
+    parser.add_argument("target", help="IP:port of the target RDP machine (ex: 192.168.1.10:3390)")
+    parser.add_argument("-l", "--listen", help="Port number to listen on (default: 3389)", default=3389)
+    parser.add_argument("-o", "--output", help="Output folder for replay files")
+    parser.add_argument("-i", "--destination-ip", help="Destination IP address of the PyRDP player.If not specified, RDP events are not sent over the network.")
+    parser.add_argument("-d", "--destination-port", help="Listening port of the PyRDP player (default: 3000).", default=3000)
     parser.add_argument("-k", "--private-key", help="Path to private key (for SSL)")
     parser.add_argument("-c", "--certificate", help="Path to certificate (for SSL)")
-    parser.add_argument("-r", "--standard-security", help="RDP standard security (XP or server 2003 client or older)",
-                        action="store_true")
-    parser.add_argument("-n", "--nla", help="For NLA client authentication (need to provide credentials)",
-                        action="store_true")
-    parser.add_argument("-u", "--username", help="Username to use to connect to the target VM (instead of the username "
-                                                 "the client sent)", default=None)
-    parser.add_argument("-p", "--password", help="Password to use to connect to the target VM (instead of the password "
-                                                 "the client sent)", default=None)
+    parser.add_argument("-n", "--nla", help="For NLA client authentication (need to provide credentials)", action="store_true")
+    parser.add_argument("-u", "--username", help="Username that will replace the client's username", default=None)
+    parser.add_argument("-p", "--password", help="Password that will replace the client's password", default=None)
     parser.add_argument("-L", "--log-level", help="Log level", default="INFO", choices=["INFO", "DEBUG", "WARNING", "ERROR", "CRITICAL"], nargs="?")
-    parser.add_argument("-s", "--sensor-id", help="Sensor ID (to differentiate multiple instances of the MITM where logs "
-                                                  "are aggregated at one place)", default="PyRDP")
+    parser.add_argument("-s", "--sensor-id", help="Sensor ID (to differentiate multiple instances of the MITM where logs are aggregated at one place)", default="PyRDP")
 
     args = parser.parse_args()
     Config.arguments = args
