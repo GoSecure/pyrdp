GoSecure/pyrdp

commit 950edf8fc29a5c0d248092ff8af804e7a0bc28f6

Re-allow multifragment updates (fixes win7 --> MITM --> win8 connections), but might introduces problems with player.

pyrdp/parser/rdp/fastpath.py
@@ -350,7 +350,7 @@ class FastPathOutputParser(Parser):
         eventType = header & 0xf
         fragmentation = header & 0b00110000 != 0
         if fragmentation:
-            log.error("Fragmentation is present in fastpath packets, it is NOT handled.")
+            log.warning("Fragmentation is present in output fastpath event packets, errors might happen during replay.")
 
         if eventType == FastPathOutputType.FASTPATH_UPDATETYPE_BITMAP:
             return self.parseBitmapEventRaw(stream, header, compressionFlags, size)

pyrdp/layer/rdp/fastpath.py
@@ -1,8 +1,9 @@
 from pyrdp.core import ObservedBy
-from pyrdp.layer.layer import LayerObserver
 from pyrdp.layer.buffered import BufferedLayer
+from pyrdp.layer.layer import LayerObserver
 from pyrdp.layer.rdp.data import RDPDataObserver
 from pyrdp.parser import SegmentationParser
+from pyrdp.pdu.rdp.fastpath import FastPathPDU
 
 
 class FastPathObserver(RDPDataObserver, LayerObserver):
@@ -10,7 +11,7 @@ class FastPathObserver(RDPDataObserver, LayerObserver):
     Observer for fast-path PDUs.
     """
 
-    def onPDUReceived(self, pdu):
+    def onPDUReceived(self, pdu: FastPathPDU):
         self.dispatchPDU(pdu)
 
     def getPDUType(self, pdu):

pyrdp/enum/rdp.py
@@ -690,3 +690,13 @@ class BitmapFlags(IntEnum):
     """
     BITMAP_COMPRESSION = 0x0001
     NO_BITMAP_COMPRESSION_HDR = 0x0400
+
+
+class FastPathFragmentation(IntEnum):
+    """
+    https://msdn.microsoft.com/en-us/library/cc240622.aspx
+    """
+    FASTPATH_FRAGMENT_SINGLE = 0x0
+    FASTPATH_FRAGMENT_LAST = 0x1
+    FASTPATH_FRAGMENT_FIRST = 0x2
+    FASTPATH_FRAGMENT_NEXT = 0x3

pyrdp/mitm/server.py
@@ -26,7 +26,7 @@ from pyrdp.mitm.virtual_channel.virtual_channel import MITMVirtualChannelObserve
 from pyrdp.parser import ClientConnectionParser, ClientInfoParser, createFastPathParser, GCCParser, \
     NegotiationRequestParser, NegotiationResponseParser, ServerConnectionParser
 from pyrdp.pdu import Capability, GCCConferenceCreateRequestPDU, GCCConferenceCreateResponsePDU, MCSConnectResponsePDU, \
-    MultifragmentUpdateCapability, NegotiationRequestPDU, NegotiationResponsePDU, ProprietaryCertificate, ServerDataPDU, \
+    NegotiationRequestPDU, NegotiationResponsePDU, ProprietaryCertificate, ServerDataPDU, \
     ServerSecurityData
 from pyrdp.recording import FileLayer, Recorder, RecordingFastPathObserver, RecordingSlowPathObserver, SocketLayer
 from pyrdp.security import RC4CrypterProxy, SecuritySettings
@@ -445,8 +445,6 @@ class MITMServer(MCSUserObserver, MCSChannelFactory):
         pdu.parsedCapabilitySets[CapabilityType.CAPSTYPE_ORDER].orderFlags = OrderFlag.NEGOTIATEORDERSUPPORT \
                                                                              | OrderFlag.ZEROBOUNDSDELTASSUPPORT
         pdu.parsedCapabilitySets[CapabilityType.CAPSTYPE_ORDER].orderSupport = b"\x00" * 32
-
-        pdu.parsedCapabilitySets[CapabilityType.CAPSETTYPE_MULTIFRAGMENTUPDATE] = MultifragmentUpdateCapability(0)
 
         # Disable virtual channel compression
         if CapabilityType.CAPSTYPE_VIRTUALCHANNEL in pdu.parsedCapabilitySets.keys():

commit 3c05a0965154e28ec3237239cbdb87f22346b590

Handles Fastpath output update events reassembly on the player side. Fixes image errors introduced in commit 950edf8fc29a5c0d248092ff8af804e7a0bc28f6

pyrdp/parser/rdp/fastpath.py
@@ -10,6 +10,7 @@ from pyrdp.parser.rdp.bitmap import BitmapParser
 from pyrdp.parser.rdp.security import BasicSecurityParser
 from pyrdp.pdu import FastPathBitmapEvent, FastPathEventRaw, FastPathMouseEvent, FastPathOrdersEvent, FastPathPDU, \
     FastPathScanCodeEvent, SecondaryDrawingOrder
+from pyrdp.pdu.rdp.fastpath import FastPathOutputUpdateEvent
 from pyrdp.security import RC4Crypter
 
 
@@ -311,32 +312,40 @@ class FastPathInputParser(Parser):
 
 
 class FastPathOutputParser(Parser):
-    def getEventLength(self, data):
-        if isinstance(data, FastPathEventRaw):
-            return len(data.data)
-        elif isinstance(data, bytes):
-            header = Uint8.unpack(data[0])
+    def __init__(self):
+        super().__init__()
+        self.bitmapParser = BitmapParser()
+
+    def getEventLength(self, event: FastPathOutputUpdateEvent):
+
+        if isinstance(event, bytes):
+            header = Uint8.unpack(event[0])
             if self.isCompressed(header):
-                return Uint16LE.unpack(data[2 : 4]) + 4
+                return Uint16LE.unpack(event[2: 4]) + 4
             else:
-                return Uint16LE.unpack(data[1 : 3]) + 3
+                return Uint16LE.unpack(event[1: 3]) + 3
 
         size = 3
 
-        if self.isCompressed(data.header):
+        if self.isCompressed(event.header):
             size += 1
 
-        if isinstance(data, FastPathOrdersEvent):
-            size += 2 + len(data.orderData)
-        elif isinstance(data, FastPathBitmapEvent):
-            size += len(data.rawBitmapUpdateData)
+        if isinstance(event, FastPathOrdersEvent):
+            size += 2 + len(event.orderData)
+        elif isinstance(event, FastPathBitmapEvent):
+            size += len(event.payload)
+        elif isinstance(event, FastPathOutputUpdateEvent):
+            length = len(event.payload) + 3
+            if event.compressionFlags is not None:
+                length += 1
+            return length
 
         return size
 
     def isCompressed(self, header):
         return (header >> 6) & FastPathOutputCompressionType.FASTPATH_OUTPUT_COMPRESSION_USED
 
-    def parse(self, data):
+    def parse(self, data) -> FastPathOutputUpdateEvent:
         stream = BytesIO(data)
         header = Uint8.unpack(stream)
 
@@ -350,32 +359,35 @@ class FastPathOutputParser(Parser):
         eventType = header & 0xf
         fragmentation = header & 0b00110000 != 0
         if fragmentation:
-            log.warning("Fragmentation is present in output fastpath event packets, errors might happen during replay.")
+            log.debug("Fragmentation is present in output fastpath event packets."
+                      " Not parsing it and saving to FastPathOutputUpdateEvent.")
+            return FastPathOutputUpdateEvent(header, compressionFlags, payload=stream.read(size))
 
         if eventType == FastPathOutputType.FASTPATH_UPDATETYPE_BITMAP:
             return self.parseBitmapEventRaw(stream, header, compressionFlags, size)
         elif eventType == FastPathOutputType.FASTPATH_UPDATETYPE_ORDERS:
             return self.parseOrdersEvent(stream, header, compressionFlags, size)
 
-        return FastPathEventRaw(data)
+        read = stream.read(size)
+        return FastPathOutputUpdateEvent(header, compressionFlags, read)
 
     def parseBitmapEventRaw(self, stream, header, compressionFlags, size):
         return FastPathBitmapEvent(header, compressionFlags, [], stream.read(size))
 
-    def parseBitmapEvent(self, fastPathBitmapEvent: FastPathBitmapEvent) -> FastPathBitmapEvent:
+    def parseBitmapEvent(self, fastPathBitmapEvent: FastPathOutputUpdateEvent) -> FastPathBitmapEvent:
         """
         :return: a FastPathBitmapEvent with bitmapUpdateData
         """
-        rawBitmapUpdateData = fastPathBitmapEvent.rawBitmapUpdateData
+        rawBitmapUpdateData = fastPathBitmapEvent.payload
         stream = BytesIO(rawBitmapUpdateData)
         updateType = Uint16LE.unpack(stream.read(2))
-        bitmapData = BitmapParser().parseBitmapUpdateData(stream.read())
+        bitmapData = self.bitmapParser.parseBitmapUpdateData(stream.read())
 
         return FastPathBitmapEvent(fastPathBitmapEvent.header, fastPathBitmapEvent.compressionFlags,
                                    bitmapData, rawBitmapUpdateData)
 
-    def writeBitmapEvent(self, stream, event):
-        stream.write(event.rawBitmapUpdateData)
+    def writeBitmapEvent(self, stream, event: FastPathBitmapEvent):
+        stream.write(event.payload)
 
     def parseOrdersEvent(self, stream, header, compressionFlags, size):
         orderCount = Uint16LE.unpack(stream)
@@ -402,9 +414,7 @@ class FastPathOutputParser(Parser):
         Uint16LE.pack(event.orderCount, stream)
         stream.write(event.orderData)
 
-    def write(self, event):
-        if isinstance(event, FastPathEventRaw):
-            return event.data
+    def write(self, event: FastPathOutputUpdateEvent):
 
         stream = BytesIO()
         Uint8.pack(event.header, stream)
@@ -418,11 +428,13 @@ class FastPathOutputParser(Parser):
             self.writeBitmapEvent(updateStream, event)
         elif isinstance(event, FastPathOrdersEvent):
             self.writeOrdersEvent(updateStream, event)
+        else:
+            # Means it's simply a FastPathOutputUpdateEvent, this needs to be the last elif.
+            updateStream.write(event.payload)
 
         updateData = updateStream.getvalue()
         Uint16LE.pack(len(updateData), stream)
         stream.write(updateData)
-
         return stream.getvalue()
 
 

pyrdp/pdu/__init__.py
@@ -3,6 +3,7 @@ from pyrdp.pdu.mcs import MCSAttachUserConfirmPDU, MCSAttachUserRequestPDU, MCSC
     MCSChannelJoinRequestPDU, MCSConnectInitialPDU, MCSConnectResponsePDU, MCSDisconnectProviderUltimatumPDU, \
     MCSDomainParams, MCSErectDomainRequestPDU, MCSPDU, MCSSendDataIndicationPDU, MCSSendDataRequestPDU
 from pyrdp.pdu.pdu import PDU
+from pyrdp.pdu.rdp.bitmap import BitmapUpdateData
 from pyrdp.pdu.rdp.capability import BitmapCacheHostSupportCapability, BitmapCacheV1Capability, BitmapCacheV2Capability, \
     BitmapCapability, BitmapCodec, BitmapCodecsCapability, BrushCapability, Capability, ClientCapsContainer, \
     ColorCacheCapability, ControlCapability, DesktopCompositionCapability, DrawGDIPlusCapability, \
@@ -12,15 +13,11 @@ from pyrdp.pdu.rdp.capability import BitmapCacheHostSupportCapability, BitmapCac
     ShareCapability, SoundCapability, SurfaceCommandsCapability, VirtualChannelCapability, WindowListCapability, \
     WindowsActivationCapability
 from pyrdp.pdu.rdp.client_info import ClientExtraInfo, ClientInfoPDU
-from pyrdp.pdu.rdp.bitmap import BitmapUpdateData
-from pyrdp.pdu.rdp.connection import ClientChannelDefinition, ClientClusterData, ClientCoreData, ClientNetworkData, \
-    ClientSecurityData, ProprietaryCertificate, ClientDataPDU, ServerDataPDU, ServerCertificate, ServerCoreData, \
+from pyrdp.pdu.rdp.connection import ClientChannelDefinition, ClientClusterData, ClientCoreData, ClientDataPDU, \
+    ClientNetworkData, ClientSecurityData, ProprietaryCertificate, ServerCertificate, ServerCoreData, ServerDataPDU, \
     ServerNetworkData, ServerSecurityData
-from pyrdp.pdu.rdp.slowpath import ConfirmActivePDU, ControlPDU, DemandActivePDU, InputPDU, PlaySoundPDU, \
-    PointerPDU, SetErrorInfoPDU, ShareControlHeader, ShareDataHeader, SuppressOutputPDU, \
-    SynchronizePDU, UpdatePDU, SlowPathPDU
 from pyrdp.pdu.rdp.fastpath import FastPathBitmapEvent, FastPathEvent, FastPathEventRaw, FastPathMouseEvent, \
-    FastPathOrdersEvent, FastPathOutputEvent, FastPathPDU, FastPathScanCodeEvent, SecondaryDrawingOrder
+    FastPathOrdersEvent, FastPathOutputUpdateEvent, FastPathPDU, FastPathScanCodeEvent, SecondaryDrawingOrder
 from pyrdp.pdu.rdp.input import ExtendedMouseEvent, KeyboardEvent, MouseEvent, SlowPathInput, SynchronizeEvent, \
     UnicodeKeyboardEvent, UnusedEvent
 from pyrdp.pdu.rdp.licensing import LicenseBinaryBlob, LicenseErrorAlertPDU, LicensingPDU
@@ -29,6 +26,8 @@ from pyrdp.pdu.rdp.pointer import Point, PointerCacheEvent, PointerColorEvent, P
     PointerPositionEvent, PointerSystemEvent
 from pyrdp.pdu.rdp.recording import PlayerMessagePDU
 from pyrdp.pdu.rdp.security import SecurityExchangePDU, SecurityPDU
+from pyrdp.pdu.rdp.slowpath import ConfirmActivePDU, ControlPDU, DemandActivePDU, InputPDU, PlaySoundPDU, PointerPDU, \
+    SetErrorInfoPDU, ShareControlHeader, ShareDataHeader, SlowPathPDU, SuppressOutputPDU, SynchronizePDU, UpdatePDU
 from pyrdp.pdu.rdp.virtual_channel.clipboard import ClipboardPDU, FormatDataRequestPDU, FormatDataResponsePDU, \
     FormatListPDU, FormatListResponsePDU, FormatName, LongFormatName, ServerMonitorReadyPDU, ShortFormatName
 from pyrdp.pdu.rdp.virtual_channel.device_redirection import DeviceAnnounce, DeviceCloseRequestPDU, \

pyrdp/pdu/rdp/fastpath.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from pyrdp.enum import SegmentationPDUType
 from pyrdp.pdu.pdu import PDU
@@ -19,14 +19,14 @@ class FastPathPDU(SegmentationPDU):
         return str([str(e.__class__) for e in self.events])
 
 
-class FastPathEvent:
+class FastPathEvent(PDU):
     """
     Base class for RDP fast path event (not PDU, a PDU contains multiple events).
     Used for scan code events, mouse events or bitmap events.
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, payload=b""):
+        super().__init__(payload)
 
 
 class FastPathEventRaw(FastPathEvent):
@@ -39,9 +39,15 @@ class FastPathInputEvent(FastPathEvent):
     def __init__(self):
         super().__init__()
 
-class FastPathOutputEvent(FastPathEvent):
-    def __init__(self):
-        super().__init__()
+
+class FastPathOutputUpdateEvent(FastPathEvent):
+    """
+    https://msdn.microsoft.com/en-us/library/cc240622.aspx
+    """
+    def __init__(self, header: int, compressionFlags: Optional[int], payload=b""):
+        super().__init__(payload)
+        self.header = header
+        self.compressionFlags = compressionFlags
 
 
 class FastPathScanCodeEvent(FastPathInputEvent):
@@ -77,23 +83,20 @@ class FastPathMouseEvent(FastPathInputEvent):
         self.pointerFlags = pointerFlags
 
 
-class FastPathBitmapEvent(FastPathOutputEvent):
+class FastPathBitmapEvent(FastPathOutputUpdateEvent):
     def __init__(self, header: int, compressionFlags: int, bitmapUpdateData: List[BitmapUpdateData],
-                 rawBitmapUpdateData: bytes):
-        super().__init__()
-        self.header = header
+                 payload: bytes):
+        super().__init__(header, compressionFlags, payload)
         self.compressionFlags = compressionFlags
-        self.rawBitmapUpdateData = rawBitmapUpdateData
         self.bitmapUpdateData = bitmapUpdateData
 
 
-class FastPathOrdersEvent(FastPathOutputEvent):
+class FastPathOrdersEvent(FastPathOutputUpdateEvent):
     """
     https://msdn.microsoft.com/en-us/library/cc241573.aspx
     """
     def __init__(self, header, compressionFlags, orderCount, orderData):
-        super().__init__()
-        self.header = header
+        super().__init__(header, compressionFlags)
         self.compressionFlags = compressionFlags
         self.orderCount = orderCount
         self.orderData = orderData

pyrdp/player/event.py
@@ -1,17 +1,19 @@
+from typing import Optional
+
 from PyQt4 import QtGui
 from PyQt4.QtGui import QTextCursor
 
 from pyrdp.core import decodeUTF16LE
 from pyrdp.core.scancode import scancodeToChar
-from pyrdp.enum import BitmapFlags, CapabilityType, KeyboardFlag, ParserMode, SlowPathUpdateType
+from pyrdp.enum import BitmapFlags, CapabilityType, FastPathFragmentation, KeyboardFlag, ParserMode, SlowPathUpdateType
 from pyrdp.layer import PlayerMessageObserver
 from pyrdp.logging import log
-from pyrdp.parser import ClipboardParser, BasicFastPathParser, BitmapParser, ClientInfoParser, SlowPathParser, \
-    FastPathOutputParser
+from pyrdp.parser import BasicFastPathParser, BitmapParser, ClientInfoParser, ClipboardParser, FastPathOutputParser, \
+    SlowPathParser
 from pyrdp.parser.rdp.connection import ClientConnectionParser
-from pyrdp.pdu import BitmapUpdateData, FastPathBitmapEvent, FastPathMouseEvent, FastPathOrdersEvent, \
-    FastPathScanCodeEvent, FormatDataResponsePDU, KeyboardEvent, MouseEvent, PlayerMessagePDU, ConfirmActivePDU, \
-    InputPDU, UpdatePDU
+from pyrdp.pdu import BitmapUpdateData, ConfirmActivePDU, FastPathBitmapEvent, FastPathMouseEvent, FastPathOrdersEvent, \
+    FastPathScanCodeEvent, FormatDataResponsePDU, InputPDU, KeyboardEvent, MouseEvent, PlayerMessagePDU, UpdatePDU
+from pyrdp.pdu.rdp.fastpath import FastPathOutputUpdateEvent
 from pyrdp.ui import RDPBitmapToQtImage
 
 
@@ -34,6 +36,8 @@ class PlayerMessageHandler(PlayerMessageObserver):
         self.outputEventParser = FastPathOutputParser()
         self.clientConnectionParser = ClientConnectionParser()
 
+        self.buffer = b""
+
     def onConnectionClose(self, pdu: PlayerMessagePDU):
         self.text.moveCursor(QTextCursor.End)
         self.text.insertPlainText("\n<Connection closed>")
@@ -42,13 +46,17 @@ class PlayerMessageHandler(PlayerMessageObserver):
         pdu = self.outputParser.parse(pdu.payload)
 
         for event in pdu.events:
-            if isinstance(event, FastPathOrdersEvent):
-                log.debug("Not handling orders event, not coded :)")
-            elif isinstance(event, FastPathBitmapEvent):
-                log.debug("Handling bitmap event %(arg1)s", {"arg1": vars(event)})
-                self.onBitmap(event)
+            reassembledEvent = self.reassembleEvent(event)
+            if reassembledEvent is not None:
+                if isinstance(reassembledEvent, FastPathOrdersEvent):
+                    log.debug("Not handling orders event, not coded :)")
+                elif isinstance(reassembledEvent, FastPathBitmapEvent):
+                    log.debug("Handling bitmap event %(arg1)s", {"arg1": type(reassembledEvent)})
+                    self.onBitmap(reassembledEvent)
+                else:
+                    log.debug("Can't handle output event: %(arg1)s", {"arg1": type(reassembledEvent)})
             else:
-                log.debug("Can't handle output event: %(arg1)s", {"arg1": event})
+                log.debug("Reassembling output event...")
 
     def onInput(self, pdu: PlayerMessagePDU):
         pdu = self.inputParser.parse(pdu.payload)
@@ -135,3 +143,23 @@ class PlayerMessageHandler(PlayerMessageObserver):
         self.text.moveCursor(QtGui.QTextCursor.End)
         self.text.insertPlainText("--------------------\n")
         self.text.insertPlainText(f"HOST: {clientDataPDU.coreData.clientName.strip(chr(0))}\n")
+
+    def reassembleEvent(self, event: FastPathOutputUpdateEvent) -> Optional[FastPathBitmapEvent]:
+        """
+        Handles FastPath event reassembly as described in
+        https://msdn.microsoft.com/en-us/library/cc240622.aspx
+        :param event: A potentially segmented fastpath output event
+        """
+        fragmentationFlag = FastPathFragmentation((event.header & 0b00110000) >> 4)
+        if fragmentationFlag == FastPathFragmentation.FASTPATH_FRAGMENT_SINGLE:
+            return event
+        elif fragmentationFlag == FastPathFragmentation.FASTPATH_FRAGMENT_FIRST:
+            self.buffer = event.payload
+        elif fragmentationFlag == FastPathFragmentation.FASTPATH_FRAGMENT_NEXT:
+            self.buffer += event.payload
+        elif fragmentationFlag == FastPathFragmentation.FASTPATH_FRAGMENT_LAST:
+            self.buffer += event.payload
+            event.payload = self.buffer
+            return self.outputEventParser.parseBitmapEvent(event)
+
+        return None