022ac95b13ae717f2d86a6723e3a0c1a57ccc0a1
Francis Labelle <flabelle@gosecure.ca>
2019-01-02 16:30:29 -0500

    Add event module

--- pyrdp/core/__init__.py
+++ pyrdp/core/__init__.py
@@ -4,9 +4,11 @@
 # Licensed under the GPLv3 or later.
 #
 
+from pyrdp.core.event import EventEngine
 from pyrdp.core.helpers import decodeUTF16LE, encodeUTF16LE, getLoggerPassFilters
-from pyrdp.core.observer import Observer, CompositeObserver
-from pyrdp.core.packing import Int8, Uint8, Int16LE, Int16BE, Uint16LE, Uint16BE, Int32LE, Int32BE, Uint32LE, Uint32BE, Uint64LE
+from pyrdp.core.observer import CompositeObserver, Observer
+from pyrdp.core.packing import Int16BE, Int16LE, Int32BE, Int32LE, Int8, Uint16BE, Uint16LE, Uint32BE, Uint32LE, \
+    Uint64LE, Uint8
 from pyrdp.core.stream import ByteStream, StrictStream
-from pyrdp.core.subject import Subject, ObservedBy
+from pyrdp.core.subject import ObservedBy, Subject
 from pyrdp.core.timer import Timer
