8accdf44c0551f7b673bf932761832cbb2f73cc7
Francis Labelle <flabelle@gosecure.ca>
2019-02-11 10:17:07 -0500

    Remove ClientConnectionLayer

--- pyrdp/layer/__init__.py
+++ pyrdp/layer/__init__.py
@@ -15,7 +15,6 @@ from pyrdp.layer.tcp import TCPObserver, TwistedTCPLayer, AsyncIOTCPLayer
 from pyrdp.layer.tpkt import TPKTLayer
 from pyrdp.layer.x224 import X224Observer, X224Layer
 
-from pyrdp.layer.rdp.connection import ClientConnectionLayer
 from pyrdp.layer.rdp.slowpath import SlowPathObserver, SlowPathLayer
 from pyrdp.layer.rdp.data import RDPDataObserver
 from pyrdp.layer.rdp.fastpath import FastPathLayer, FastPathObserver
