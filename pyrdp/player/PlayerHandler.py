0fb2013c165bb6a09641ea17686192b2c0a219de
Francis Labelle <flabelle@gosecure.ca>
2019-03-28 18:08:02 -0400

    Refactor scan code handling

--- pyrdp/player/PlayerHandler.py
+++ pyrdp/player/PlayerHandler.py
@@ -10,7 +10,6 @@ from PySide2.QtGui import QTextCursor
 from PySide2.QtWidgets import QTextEdit
 
 from pyrdp.core import decodeUTF16LE
-from pyrdp.core.scancode import scancodeToChar
 from pyrdp.enum import BitmapFlags, CapabilityType, FastPathFragmentation, KeyboardFlag, ParserMode, SlowPathUpdateType
 from pyrdp.layer import PlayerObserver
 from pyrdp.logging import log
@@ -19,6 +18,7 @@ from pyrdp.parser import BasicFastPathParser, BitmapParser, ClientConnectionPars
 from pyrdp.pdu import BitmapUpdateData, ConfirmActivePDU, FastPathBitmapEvent, FastPathMouseEvent, FastPathOrdersEvent, \
     FastPathOutputEvent, FastPathScanCodeEvent, FastPathUnicodeEvent, FormatDataResponsePDU, InputPDU, KeyboardEvent, \
     MouseEvent, PlayerPDU, UpdatePDU
+from pyrdp.player import keyboard
 from pyrdp.ui import QRemoteDesktop, RDPBitmapToQtImage
 
 
@@ -31,6 +31,8 @@ class PlayerHandler(PlayerObserver):
         super().__init__()
         self.viewer = viewer
         self.text = text
+        self.shiftPressed = False
+        self.capsLockOn = False
         self.writeInCaps = False
 
         self.inputParser = BasicFastPathParser(ParserMode.SERVER)
@@ -73,36 +75,50 @@ class PlayerHandler(PlayerObserver):
         for event in pdu.events:
             if isinstance(event, FastPathScanCodeEvent):
                 log.debug("handling %(arg1)s", {"arg1": event})
-                self.onScanCode(event.scancode, not event.isReleased)
-            elif isinstance(event, FastPathUnicodeEvent) and not event.released:
-                self.onUnicode(event)
+                self.onScanCode(event.scancode, event.isReleased, event.rawHeaderByte & 2 != 0)
+            elif isinstance(event, FastPathUnicodeEvent):
+                if not event.released:
+                    self.onUnicode(event)
             elif isinstance(event, FastPathMouseEvent):
-                self.onMousePosition(event.mouseX, event.mouseY)
+                self.onMouse(event)
             else:
                 log.debug("Can't handle input event: %(arg1)s", {"arg1": event})
 
-    def onScanCode(self, code: int, isPressed: bool):
+
+    def onScanCode(self, scanCode: int, isReleased: bool, isExtended: bool):
         """
         Handle scan code.
         """
-        log.debug("Reading scancode %(arg1)s", {"arg1": code})
+        log.debug("Reading scan code %(arg1)s", {"arg1": scanCode})
+        keyName = keyboard.getKeyName(scanCode, isExtended, self.shiftPressed, self.capsLockOn)
 
-        if code in [0x2A, 0x36]:
+        self.text.moveCursor(QTextCursor.End)
+
+        if len(keyName) == 1:
+            if not isReleased:
+                self.text.insertPlainText(keyName)
+        else:
+            self.text.insertPlainText(f"\n<{keyName} {'released' if isReleased else 'pressed'}>")
+
+        self.text.moveCursor(QTextCursor.End)
+
+        # Left or right shift
+        if scanCode in [0x2A, 0x36]:
             self.text.moveCursor(QTextCursor.End)
-            self.text.insertPlainText("\n<LSHIFT PRESSED>" if isPressed else "\n<LSHIFT RELEASED>")
-            self.writeInCaps = not self.writeInCaps
-        elif code == 0x3A and isPressed:
+            self.shiftPressed = not isReleased
+
+        # Caps lock
+        elif scanCode == 0x3A and not isReleased:
             self.text.moveCursor(QTextCursor.End)
-            self.text.insertPlainText("\n<CAPSLOCK>")
-            self.writeInCaps = not self.writeInCaps
-        elif isPressed:
-            char = scancodeToChar(code)
-            self.text.moveCursor(QTextCursor.End)
-            self.text.insertPlainText(char if self.writeInCaps else char.lower())
+            self.capsLockOn = not self.capsLockOn
+
 
     def onUnicode(self, event: FastPathUnicodeEvent):
         self.text.moveCursor(QTextCursor.End)
         self.text.insertPlainText(str(event.text))
+
+    def onMouse(self, event: FastPathMouseEvent):
+        self.onMousePosition(event.mouseX, event.mouseY)
 
     def onMousePosition(self, x: int, y: int):
         self.viewer.setMousePosition(x, y)
@@ -141,7 +157,7 @@ class PlayerHandler(PlayerObserver):
                 if isinstance(event, MouseEvent):
                     self.onMousePosition(event.x, event.y)
                 elif isinstance(event, KeyboardEvent):
-                    self.onScanCode(event.keyCode, event.flags & KeyboardFlag.KBDFLAGS_DOWN != 0)
+                    self.onScanCode(event.keyCode, event.flags & KeyboardFlag.KBDFLAGS_DOWN == 0, event.flags & KeyboardFlag.KBDFLAGS_EXTENDED != 0)
 
     def onClipboardData(self, pdu: PlayerPDU):
         formatDataResponsePDU: FormatDataResponsePDU = self.clipboardParser.parse(pdu.payload)
--- pyrdp/player/RDPMITMWidget.py
+++ pyrdp/player/RDPMITMWidget.py
@@ -12,6 +12,8 @@ from pyrdp.enum import MouseButton
 from pyrdp.layer import PlayerLayer
 from pyrdp.logging import LOGGER_NAMES
 from pyrdp.pdu import PlayerKeyboardPDU, PlayerMouseButtonPDU, PlayerMouseMovePDU, PlayerMouseWheelPDU, PlayerTextPDU
+from pyrdp.player import keyboard
+from pyrdp.player.keyboard import isRightControl
 from pyrdp.player.Sequencer import Sequencer
 from pyrdp.ui import QRemoteDesktop
 
@@ -21,171 +23,7 @@ class RDPMITMWidget(QRemoteDesktop):
     RDP Widget that handles mouse and keyboard events and sends them to the MITM server.
     """
 
-    SCANCODE_MAPPING = {
-        Qt.Key.Key_Escape: 0x01,
-        Qt.Key.Key_1: 0x02,
-        Qt.Key.Key_Exclam: 0x02,
-        Qt.Key.Key_2: 0x03,
-        Qt.Key.Key_At: 0x03,
-        Qt.Key.Key_3: 0x04,
-        Qt.Key.Key_NumberSign: 0x04,
-        Qt.Key.Key_4: 0x05,
-        Qt.Key.Key_Dollar: 0x05,
-        Qt.Key.Key_5: 0x06,
-        Qt.Key.Key_Percent: 0x06,
-        Qt.Key.Key_6: 0x07,
-        Qt.Key.Key_AsciiCircum: 0x07,
-        Qt.Key.Key_7: 0x08,
-        Qt.Key.Key_Ampersand: 0x08,
-        Qt.Key.Key_8: 0x09,
-        Qt.Key.Key_Asterisk: 0x09,
-        Qt.Key.Key_9: 0x0A,
-        Qt.Key.Key_ParenLeft: 0x0A,
-        Qt.Key.Key_0: 0x0B,
-        Qt.Key.Key_ParenRight: 0x0B,
-        Qt.Key.Key_Minus: 0x0C,
-        Qt.Key.Key_Underscore: 0x0C,
-        Qt.Key.Key_Equal: 0x0D,
-        Qt.Key.Key_Plus: 0x0D,
-        Qt.Key.Key_Backspace: 0x0E,
-        Qt.Key.Key_Tab: 0x0F,
-        Qt.Key.Key_Q: 0x10,
-        Qt.Key.Key_W: 0x11,
-        Qt.Key.Key_E: 0x12,
-        Qt.Key.Key_R: 0x13,
-        Qt.Key.Key_T: 0x14,
-        Qt.Key.Key_Y: 0x15,
-        Qt.Key.Key_U: 0x16,
-        Qt.Key.Key_I: 0x17,
-        Qt.Key.Key_O: 0x18,
-        Qt.Key.Key_P: 0x19,
-        Qt.Key.Key_BracketLeft: 0x1A,
-        Qt.Key.Key_BraceLeft: 0x1A,
-        Qt.Key.Key_BracketRight: 0x1B,
-        Qt.Key.Key_BraceRight: 0x1B,
-        Qt.Key.Key_Return: 0x1C,
-        Qt.Key.Key_Control: 0x1D,
-        Qt.Key.Key_A: 0x1E,
-        Qt.Key.Key_S: 0x1F,
-        Qt.Key.Key_D: 0x20,
-        Qt.Key.Key_F: 0x21,
-        Qt.Key.Key_G: 0x22,
-        Qt.Key.Key_H: 0x23,
-        Qt.Key.Key_J: 0x24,
-        Qt.Key.Key_K: 0x25,
-        Qt.Key.Key_L: 0x26,
-        Qt.Key.Key_Semicolon: 0x27,
-        Qt.Key.Key_Colon: 0x27,
-        Qt.Key.Key_Apostrophe: 0x28,
-        Qt.Key.Key_QuoteDbl: 0x28,
-        Qt.Key.Key_QuoteLeft: 0x29,
-        Qt.Key.Key_AsciiTilde: 0x29,
-        Qt.Key.Key_Shift: 0x2A,
-        Qt.Key.Key_Backslash: 0x2B,
-        Qt.Key.Key_Bar: 0x2B,
-        Qt.Key.Key_Z: 0x2C,
-        Qt.Key.Key_X: 0x2D,
-        Qt.Key.Key_C: 0x2E,
-        Qt.Key.Key_V: 0x2F,
-        Qt.Key.Key_B: 0x30,
-        Qt.Key.Key_N: 0x31,
-        Qt.Key.Key_M: 0x32,
-        Qt.Key.Key_Comma: 0x33,
-        Qt.Key.Key_Less: 0x33,
-        Qt.Key.Key_Period: 0x34,
-        Qt.Key.Key_Greater: 0x34,
-        Qt.Key.Key_Slash: 0x35,
-        Qt.Key.Key_Question: 0x35,
-        Qt.Key.Key_Print: 0x37,
-        Qt.Key.Key_Alt: 0x38,
-        Qt.Key.Key_AltGr: 0x38,
-        Qt.Key.Key_Space: 0x39,
-        Qt.Key.Key_CapsLock: 0x3A,
-        Qt.Key.Key_F1: 0x3B,
-        Qt.Key.Key_F2: 0x3C,
-        Qt.Key.Key_F3: 0x3D,
-        Qt.Key.Key_F4: 0x3E,
-        Qt.Key.Key_F5: 0x3F,
-        Qt.Key.Key_F6: 0x40,
-        Qt.Key.Key_F7: 0x41,
-        Qt.Key.Key_F8: 0x42,
-        Qt.Key.Key_F9: 0x43,
-        Qt.Key.Key_F10: 0x44,
-        Qt.Key.Key_NumLock: 0x45,
-        Qt.Key.Key_ScrollLock: 0x46,
-        Qt.Key.Key_Home: 0x47,
-        Qt.Key.Key_Up: 0x48,
-        Qt.Key.Key_PageUp: 0x49,
-        Qt.Key.Key_Left: 0x4B,
-        Qt.Key.Key_Right: 0x4D,
-        Qt.Key.Key_End: 0x4F,
-        Qt.Key.Key_Down: 0x50,
-        Qt.Key.Key_PageDown: 0x51,
-        Qt.Key.Key_Insert: 0x52,
-        Qt.Key.Key_Delete: 0x53,
-        Qt.Key.Key_SysReq: 0x54,
-        Qt.Key.Key_F11: 0x57,
-        Qt.Key.Key_F12: 0x58,
-        Qt.Key.Key_Meta: 0x5B,
-        Qt.Key.Key_Menu: 0x5D,
-        Qt.Key.Key_Sleep: 0x5F,
-        Qt.Key.Key_Zoom: 0x62,
-        Qt.Key.Key_Help: 0x63,
-        Qt.Key.Key_F13: 0x64,
-        Qt.Key.Key_F14: 0x65,
-        Qt.Key.Key_F15: 0x66,
-        Qt.Key.Key_F16: 0x67,
-        Qt.Key.Key_F17: 0x68,
-        Qt.Key.Key_F18: 0x69,
-        Qt.Key.Key_F19: 0x6A,
-        Qt.Key.Key_F20: 0x6B,
-        Qt.Key.Key_F21: 0x6C,
-        Qt.Key.Key_F22: 0x6D,
-        Qt.Key.Key_F23: 0x6E,
-        Qt.Key.Key_F24: 0x6F,
-        Qt.Key.Key_Hiragana: 0x70,
-        Qt.Key.Key_Kanji: 0x71,
-        Qt.Key.Key_Hangul: 0x72,
-    }
-
-    SCANCODE_MAPPING_NUMPAD = {
-        Qt.Key.Key_Enter: 0x1C,
-        Qt.Key.Key_Slash: 0x35,
-        Qt.Key.Key_Asterisk: 0x37,
-        Qt.Key.Key_7: 0x47,
-        Qt.Key.Key_8: 0x48,
-        Qt.Key.Key_9: 0x49,
-        Qt.Key.Key_Minus: 0x4A,
-        Qt.Key.Key_4: 0x4B,
-        Qt.Key.Key_5: 0x4C,
-        Qt.Key.Key_6: 0x4D,
-        Qt.Key.Key_Plus: 0x4E,
-        Qt.Key.Key_1: 0x4F,
-        Qt.Key.Key_2: 0x50,
-        Qt.Key.Key_3: 0x51,
-        Qt.Key.Key_0: 0x52,
-        Qt.Key.Key_Period: 0x53,
-    }
-
-    EXTENDED_KEYS = [
-        Qt.Key.Key_Meta,
-        Qt.Key.Key_AltGr,
-        Qt.Key.Key_PageUp,
-        Qt.Key.Key_PageDown,
-        Qt.Key.Key_Insert,
-        Qt.Key.Key_Delete,
-        Qt.Key.Key_Home,
-        Qt.Key.Key_End,
-        Qt.Key.Key_Print,
-        Qt.Key.Key_Left,
-        Qt.Key.Key_Right,
-        Qt.Key.Key_Up,
-        Qt.Key.Key_Down,
-        Qt.Key.Key_Menu,
-    ]
-
     KEY_SEQUENCE_DELAY = 0
-
 
     def __init__(self, width: int, height: int, layer: PlayerLayer, parent: Optional[QWidget] = None):
         super().__init__(width, height, parent = parent)
@@ -234,6 +72,7 @@ class RDPMITMWidget(QRemoteDesktop):
         pdu = PlayerMouseButtonPDU(self.getTimetamp(), x, y, mapping[button], pressed)
         self.layer.sendPDU(pdu)
 
+
     def wheelEvent(self, event: QWheelEvent):
         x, y = self.getMousePosition(event)
         delta = event.delta()
@@ -253,20 +92,9 @@ class RDPMITMWidget(QRemoteDesktop):
 
         return QObject.eventFilter(self, obj, event)
 
-    def findScanCodeForEvent(self, event: QKeyEvent) -> Optional[int]:
-        if event.modifiers() & Qt.KeypadModifier != 0:
-            mapping = RDPMITMWidget.SCANCODE_MAPPING_NUMPAD
-        else:
-            mapping = RDPMITMWidget.SCANCODE_MAPPING
-
-        key = event.key()
-        return mapping.get(key, None)
-
-    def isRightControl(self, event: QKeyEvent):
-        return event.key() == Qt.Key.Key_Control and event.nativeScanCode() > 50
 
     def keyPressEvent(self, event: QKeyEvent):
-        if not self.isRightControl(event):
+        if not isRightControl(event):
             self.handleKeyEvent(event, False)
         else:
             self.clearFocus()
@@ -281,23 +109,24 @@ class RDPMITMWidget(QRemoteDesktop):
         else:
             offset = 0
 
-        scanCode = self.findScanCodeForEvent(event) or event.nativeScanCode() + offset
-        pdu = PlayerKeyboardPDU(self.getTimetamp(), scanCode, released, event.key() in RDPMITMWidget.EXTENDED_KEYS)
+        scanCode = keyboard.findScanCodeForEvent(event) or event.nativeScanCode() + offset
+        pdu = PlayerKeyboardPDU(self.getTimetamp(), scanCode, released, event.key() in keyboard.EXTENDED_KEYS)
         self.layer.sendPDU(pdu)
+
 
     def sendKeySequence(self, keys: [Qt.Key]):
         pressPDUs = []
         releasePDUs = []
 
         for key in keys:
-            scanCode = RDPMITMWidget.SCANCODE_MAPPING[key]
-            isExtended = key in RDPMITMWidget.EXTENDED_KEYS
+            scanCode = keyboard.SCANCODE_MAPPING[key]
+            isExtended = key in keyboard.EXTENDED_KEYS
 
-            pdu = PlayerKeyboardPDU(self.getTimetamp(), scanCode, False, isExtended)
-            pressPDUs.append(pdu)
+            pressPDU = PlayerKeyboardPDU(self.getTimetamp(), scanCode, False, isExtended)
+            pressPDUs.append(pressPDU)
 
-            pdu = PlayerKeyboardPDU(self.getTimetamp(), scanCode, True, isExtended)
-            releasePDUs.append(pdu)
+            releasePDU = PlayerKeyboardPDU(self.getTimetamp(), scanCode, True, isExtended)
+            releasePDUs.append(releasePDU)
 
         def press() -> int:
             for pdu in pressPDUs:
@@ -312,10 +141,11 @@ class RDPMITMWidget(QRemoteDesktop):
         sequencer = Sequencer([press, release])
         sequencer.run()
 
+
     def sendText(self, text: str):
         functions = []
 
-        def pressCharacter(character: str):
+        def pressCharacter(character: str) -> int:
             pdu = PlayerTextPDU(self.getTimetamp(), character, False)
             print(c)
             self.layer.sendPDU(pdu)
