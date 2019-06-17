70b9aaf0e80c11508514b61049fa4a1d4c62c8f8
Pourliver <maximecarbonneau1@hotmail.com>
2019-06-17 15:10:32 -0400

    Added some options to player, and live connection count

--- pyrdp/player/LiveWindow.py
+++ pyrdp/player/LiveWindow.py
@@ -6,9 +6,10 @@
 
 import asyncio
 from queue import Queue
+from typing import Dict
 
-from PySide2.QtCore import Signal, Qt
-from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtCore import Qt, Signal
+from PySide2.QtWidgets import QApplication, QMessageBox, QWidget
 
 from pyrdp.player.BaseWindow import BaseWindow
 from pyrdp.player.LiveTab import LiveTab
@@ -19,16 +20,20 @@ class LiveWindow(BaseWindow):
     """
     Class that holds logic for live player (network RDP connections as they happen) tabs.
     """
-    connectionReceived = Signal()
 
-    def __init__(self, address: str, port: int, parent: QWidget = None):
-        super().__init__(parent)
+    connectionReceived = Signal()
+    closedTabText = " - Closed"
+
+    def __init__(self, address: str, port: int, updateCountSignal: Signal, options: Dict[str, object], parent: QWidget = None):
+        super().__init__(options, parent)
+
         QApplication.instance().aboutToQuit.connect(self.onClose)
 
         self.server = LiveThread(address, port, self.onConnection)
         self.server.start()
         self.connectionReceived.connect(self.createLivePlayerTab)
         self.queue = Queue()
+        self.updateCountSignal = updateCountSignal
 
     def onConnection(self) -> asyncio.Protocol:
         self.connectionReceived.emit()
@@ -39,14 +44,18 @@ class LiveWindow(BaseWindow):
         tab = LiveTab()
         tab.connectionRenameTab.connect(self.renameLivePlayerTab)
         self.addTab(tab, "New connection")
-        self.setCurrentIndex(self.count() - 1)
+
+        if self.options.get("focusNewTab"):
+            self.setCurrentIndex(self.count() - 1)
+
+        self.updateCountSignal.emit()
         self.queue.put(tab)
 
     def renameLivePlayerTab(self, tab: LiveTab, name: str, closed: bool):
         index = self.indexOf(tab)
         if closed:
             text = self.tabText(index)
-            self.setTabText(index, text + " - Closed")
+            self.setTabText(index, text + self.closedTabText)
         elif name:
             self.setTabText(index, name)
 
@@ -64,3 +73,24 @@ class LiveWindow(BaseWindow):
 
         if tab is not None:
             tab.sendText(text)
+
+    def onTabClosed(self, index):
+        """
+        Gracefully closes the tab by calling the onClose method
+        :param index: Index of the closed tab
+        """
+        super().onTabClosed(index)
+        self.updateCountSignal.emit()
+
+    def onTabCloseRequest(self, index):
+        """
+        Prompt the user for validation when the connection is live, then forward call to the parent.
+        """
+        text = self.tabText(index)
+
+        if not text.endswith(self.closedTabText):
+            reply = QMessageBox.question(self, "Confirm close", "Are you sure you want to close a tab with an active connection?", QMessageBox.Yes|QMessageBox.No)
+            if reply == QMessageBox.No:
+                return
+
+        super().onTabCloseRequest(index)
