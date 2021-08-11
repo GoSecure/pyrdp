5411be5e3365d95f563a81bd46fe346904f6cac1
Olivier Bilodeau <obilodeau@gosecure.net>
2021-08-11 14:10:38 -0400

    NLA-redirection fix for #342: rely on host and port
    We relied only on the host to determine if a client was being redirected. Using host/port makes NLA redirection work in useful test situations.

--- pyrdp/mitm/state.py
+++ pyrdp/mitm/state.py
@@ -118,7 +118,7 @@ class RDPMITMState:
         return None not in [self.config.redirectionHost, self.config.redirectionPort] and not self.isRedirected()
 
     def isRedirected(self) -> bool:
-        return self.effectiveTargetHost == self.config.redirectionHost
+        return self.effectiveTargetHost == self.config.redirectionHost and self.effectiveTargetPort == self.config.redirectionPort
 
     def useRedirectionHost(self):
         self.effectiveTargetHost = self.config.redirectionHost
