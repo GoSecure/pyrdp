da223b8a04d20d44ecb05e8b3d576b5e376e2c68
Olivier Bilodeau <obilodeau@gosecure.net>
2022-01-06 09:32:53 -0500

    fixup: PCAP to JSON test was broken, validator needed tweaking too

--- test/validate_json.sh
+++ test/validate_json.sh
@@ -5,7 +5,7 @@ if [[ `cat $@ | jq -e ".info | length"` -lt 4 ]]; then
   exit 1
 fi
 
-if [[ `cat $@ | jq -e ".events | length"` -ne 25 ]]; then
+if [[ `cat $@ | jq -e ".events | length"` -lt 25 ]]; then
   echo "JSON conversion failed: not enough events"
   exit 2
 fi
