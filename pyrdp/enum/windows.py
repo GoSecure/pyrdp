240b6d047928de0cc74be63604c0904d45913c01
Olivier Bilodeau <obilodeau@gosecure.net>
2022-11-15 14:48:44 -0500

    Added NTSTATUS' STATUS_ACCESS_DENIED enum member
    Encountered in tests

--- pyrdp/enum/windows.py
+++ pyrdp/enum/windows.py
@@ -28,6 +28,7 @@ class NTSTATUS(IntEnum):
     [MS-ERREF]: Windows Error Codes
     https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/87fba13e-bf06-450e-83b1-9241dc81e781
     """
-    STATUS_SUCCESS = 0x00000000
+    STATUS_SUCCESS       = 0x00000000
     STATUS_NO_MORE_FILES = 0x80000006
-    STATUS_NO_SUCH_FILE = 0xC000000F
+    STATUS_NO_SUCH_FILE  = 0xC000000F
+    STATUS_ACCESS_DENIED = 0xC0000022
