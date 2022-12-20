76e80569b36ef17c5fbebcc9def4e65c5840c060
Olivier Bilodeau <obilodeau@gosecure.net>
2022-12-20 16:54:45 -0500

    Copyright year bump

--- bin/pyrdp-convert.py
+++ bin/pyrdp-convert.py
@@ -2,7 +2,7 @@
 
 #
 # This file is part of the PyRDP project.
-# Copyright (C) 2020-2021 GoSecure Inc.
+# Copyright (C) 2020-2022 GoSecure Inc.
 # Licensed under the GPLv3 or later.
 #
 import argparse
